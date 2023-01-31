0cc0064d94fafd50b520af5242cc35dfa814aa86
Dhruvsr <dhruvramu@gmail.com>
2023-01-31 13:57:25 +0530

    Backend: main.ts and app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './services/prisma/prisma.service';
+import { AuthController } from './routes/auth/auth.controller';
+import { APP_PIPE } from '@nestjs/core';
+import { ZodValidationPipe } from 'nestjs-zod';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AuthController],
+  providers: [
+    AppService,
+    PrismaService,
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
