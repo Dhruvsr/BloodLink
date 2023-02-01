54bada2f090ba88ededd9ba89a0ba37d85f42512
Dhruvsr <dhruvramu@gmail.com>
2023-02-01 20:24:54 +0530

    [frontend] Create user hook

---
+++ frontend/src/context/user.tsx
@@ -0,0 +1,16 @@
+import { create } from "zustand"
+
+interface UserState {
+    avatarUrl: string;
+    id: string;
+    role: "Donor" | "Patient";
+}
+
+export const useUser = create<{ user: UserState, setUser: (user: Partial<UserState>) => void }>(set => ({
+    user: {
+        avatarUrl: "", id: "", role: "Donor"
+    },
+    setUser(user) {
+        set(state => ({ user: { ...state.user, ...user } }))
+    },
+}))
