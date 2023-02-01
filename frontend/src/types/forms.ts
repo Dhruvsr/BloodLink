fdff04de74772b693d70350e32bfd73009ed5abb
Dhruvsr <dhruvramu@gmail.com>
2023-02-01 20:30:41 +0530

    [frontend] Create types for donor

---
+++ frontend/src/types/forms.ts
@@ -0,0 +1,23 @@
+const DonorInfo = {
+    address: "Test",
+    phone: "5555555555",
+    email: "test@gmail.com",
+    name: "Test User",
+    aadharNumber: "888888888888",
+    allergies: [],
+    bloodGroup: "O+",
+    currentHealthStatus: "healthy",
+    dateOfBirth: "2023-01-02T18:30:00.000Z",
+    donatedBloodPreviously: false,
+    drugUseHistory: [],
+    gender: "Male",
+    height: "29",
+    medicationsCurrentlyTaking: [],
+    pregnant: false,
+    weight: "10000",
+    previousBloodDonationDates: ["2023-01-03T18:30:00.000Z"],
+    travelHistory: [],
+  };
+  
+  export type DonorInfo = typeof DonorInfo;
+  
