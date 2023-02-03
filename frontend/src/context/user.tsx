97e03d4609ee7bf72da8e4bbdc83b5c154a6b6bc
Dhruvsr <dhruvramu@gmail.com>
2023-02-04 01:22:37 +0530

    [frontend] add name field to user

--- frontend/src/context/user.tsx
+++ frontend/src/context/user.tsx
@@ -1,16 +1,22 @@
-import { create } from "zustand"
+import { create } from "zustand";
 
 interface UserState {
-    avatarUrl: string;
-    id: string;
-    role: "Donor" | "Patient";
+  avatarUrl: string;
+  id: string;
+  role: "Donor" | "Patient";
+  name?: string;
 }
 
-export const useUser = create<{ user: UserState, setUser: (user: Partial<UserState>) => void }>(set => ({
-    user: {
-        avatarUrl: "", id: "", role: "Donor"
-    },
-    setUser(user) {
-        set(state => ({ user: { ...state.user, ...user } }))
-    },
-}))
+export const useUser = create<{
+  user: UserState;
+  setUser: (user: Partial<UserState>) => void;
+}>((set) => ({
+  user: {
+    avatarUrl: "",
+    id: "",
+    role: "Donor",
+  },
+  setUser(user) {
+    set((state) => ({ user: { ...state.user, ...user } }));
+  },
+}));
